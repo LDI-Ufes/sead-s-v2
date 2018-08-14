6eda72a75845c34a0d969b5ce5996f2228293a4c
Unknown <isabelazucarato@designinstrucional.org>
2018-08-14 17:52:07 -0300

    Criando a página de listagem dos posts Blog
    Renomeando a página Blog anterior para category-blog.

--- page-blog.php
+++ page-blog.php
@@ -8,18 +8,18 @@
     <meta property="creator.productor" content="http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/425">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/sead-v2/style.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/sead-v2/css/blog.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/sead-v2/css/blog-page.css">
     
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700" rel="stylesheet"> <!-- Conferir necessidade de variações -->
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
   </head>
   <body>
-      <header>
+<!--      <header>
           <div id="topoBlog">
             <?php include 'svg/nossoBLOG.svg' ?>
 
-<!--            <ul>
+            <ul>
               <li>Portal Sead
                   <ul>
                       <li>Início</li>
@@ -30,49 +30,47 @@
                       <li>Notícias</li>
                   </ul>
               </li>
-            </ul>-->
+            </ul>
         </div>
-      </header>
+      </header>-->
       
-      <div class="post-capa gradient">
-        <img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG_20180621_212726229.jpg">
-      </div>
       <main>
-        <article>
-            <h1>A gestão do design no âmbito da produção de materiais didáticos para o ensino a distância</h1>
-            <div class="post-date"><i class="fas fa-clock"></i>28 de julho de 2018</div>
-
-            <div class="post-body">
-              <p>A Educação a Distância (EaD) é entendida pelo Decreto nº 5.622, de 19 de dezembro de 2005, como modalidade educacional mediada pelas tecnologias de informação e comunicação, em que os envolvidos desenvolvem atividades em tempos e lugares diversos. O Decreto nº 5.800, de 8 de junho de 2006, instituiu o Sistema Universidade Aberta do Brasil (UAB), que incentivou a expansão dos cursos superiores no país e fortaleceu a EaD. </p>
-              <blockquote>O LDI é responsável pela produção de materiais de apoio aos cursos na modalidade a distância da universidade, coordenado por professores do curso de Design, em parceria e colaboração com professores, designers instrucionais e coordenadores dos cursos distância. Executor de centenas de projetos para a EAD, o laboratório conta com diversas equipes, sendo uma delas encarregada de gerir as atividades, o fluxo de trabalho e os projetos.</blockquote>
-              <p>No cenário de desenvolvimento da Educação a Distância, a literatura e os relatos de experiência enfatizam que ocorreram mudanças significativas ao longo de todo o processo educacional. Nesse contexto, a complexa tarefa de desenvolvimento de materiais na contemporaneidade deve se pautar na reconfiguração dos processos de ensino-aprendizagem, que destaca o estudante em uma posição privilegiada na construção do próprio conhecimento. A EAD enfrentou e ainda enfrenta desafios no que concerne ao desenvolvimento de recursos educacionais, que usualmente ocorre com a mediação e colaboração entre professores, tutores, designers instrucionais, coordenadores de cursos, dentre outros agentes.</p>
-              <img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/editada03.jpg">
-            </div>
-
-            <div class="post-autor">
-                <img class="autor-avatar" src="https://ldi.eadufes.org/wp-content/uploads/2017/03/leticiapedruzzi.jpg">
-                <div class="autor-info">
-                  postado por
-                  <div class="autor-name">Letícia Pedruzi</div>
-                  <div class="autor-role">Coordenadora do Laboratório de design instrucional-SEAD</div>
-                  <div class="autor-bio">Professora do Departamento de Desenho Industrial da Universidade Federal do Espírito Santo e coordenadora de gestão e diagramação do Laboratório de Design Instrucional.</div>             
-                </div>
-            </div>
-        </article>
+          <ul id="posts">
+              <li class="blog-post">
+                  <div class="blog-post-header">
+                    <span>Seminários</span>
+                    <img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG_20180621_212726229.jpg">
+                    <div class="post-date">28 de julho de 2018</div>
+                  </div>
+                <h3>A gestão do design no âmbito da produção de materiais didáticos para o ensino a distância</h1>
+                <p>A Educação a Distância (EaD) é entendida pelo Decreto nº 5.622, de 19 de dezembro de 2005, como modalidade educacional mediada pelas tecnologias de informação e comunicação, em que os envolvidos desenvolvem atividades em tempos e lugares diversos. </p>
+              </li>
+              
+              <li class="blog-post">
+                  <div class="blog-post-header">
+                    <span>Seminários</span>
+                    <img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG_20180621_212726229.jpg">
+                    <div class="post-date">28 de julho de 2018</div>
+                  </div>
+                <h3>A gestão do design no âmbito da produção de materiais didáticos para o ensino a distância</h1>
+                <p>A Educação a Distância (EaD) é entendida pelo Decreto nº 5.622, de 19 de dezembro de 2005, como modalidade educacional mediada pelas tecnologias de informação e comunicação, em que os envolvidos desenvolvem atividades em tempos e lugares diversos. </p>
+              </li>
+          </ul>
+          
+          <section id="ultimos-editais">
+              <div class="box-title"><span>#</span><h2>últimos<br>editais</h2></div>
+            <ul>
+                <li>
+                    <h3>Edital n°003/2018</h3>
+                    <em>inscrições abertas</em>
+                    <p class="atribuicao">Professores Orientadores (TCC)</p>
+                    <p>Especialização em Oratória, Transversalidade e Didática da Fala para Formação de Professores</p>
+                </li>
+            </ul>
+          </section>
       </main>
+ 
           
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!--    <script>
-        $(window).on("scroll", function () {
-            if ($(this).scrollTop() === 0) {
-                $("#topoBlog svg").css("height", 200);
-                $(".post-capa").addClass('gradient');
-            }
-            else {
-               $("#topoBlog svg").css("height", 64);
-               $(".post-capa").removeClass('gradient');
-            }
-        });
-    </script>-->
   </body>
 </html>
