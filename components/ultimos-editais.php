a784961f2d93404c220e3243bd76542ceb861bfc
Unknown <isabelazucarato@designinstrucional.org>
2018-09-17 12:19:28 -0300

    Alimentação do componente Últimos Editais via wp

--- components/ultimos-editais.php
+++ components/ultimos-editais.php
@@ -1,7 +1,7 @@
 <section id="ultimos-editais"> <!-- componente -->
     <div class="box-title"><span>#</span><h2>&nbsp últimos<br>editais</h2></div>
     <ul>
-        <li>
+<!--        <li>
             <h3><a href="">Edital n°003/2018</a></h3>
             <em>inscrições abertas</em>
             <p class="atribuicao">Professores Orientadores (TCC)</p>
@@ -12,6 +12,19 @@
             <em>inscrições abertas</em>
             <p class="atribuicao">Professores Orientadores (TCC)</p>
             <p>Especialização em Oratória, Transversalidade e Didática da Fala para Formação de Professores</p>
-        </li>
+        </li>-->
+        <?php           
+            $ultimos_editais = new WP_Query( array( 'post_type' => 'edital', 'posts_per_page' => 4 ) );
+            
+            if ( $ultimos_editais->have_posts() ) : while ( $ultimos_editais->have_posts() ) : $ultimos_editais->the_post(); ?>
+                <li>
+                    <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+                    <p class="atribuicao"><?php echo strip_tags(get_the_term_list( $post->ID, 'atribuicao' )); ?></p>
+                    <p><?php echo strip_tags(get_the_term_list( $post->ID, 'tipo-de-curso' )); ?>: <?php echo strip_tags(get_the_term_list( $post->ID, 'curso' )); ?></p>
+                </li>
+        <?php endwhile; wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p><?php esc_html_e( 'Sem notícias.' ); ?></p>
+        <?php endif; ?>
     </ul>
 </section>
