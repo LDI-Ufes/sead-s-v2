d58dc897d9d020cd7a921a227d508b41b2f70817
Unknown <isabelazucarato@designinstrucional.org>
2018-09-19 12:54:27 -0300

    Criando página Galeria

---
+++ page-galeria.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Galeria | Sead Ufes</title>
+
+    </li> Contexto do órgão no Portal de Serviços -->
+    <meta property="creator.productor" content="http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/425">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/sead-v2/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/sead-v2/css/page-galeria.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,600,700,700i" rel="stylesheet"> </li> Conferir necessidade de variações -->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700i" rel="stylesheet"> </li> Conferir necessidade de variações -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+  </head>
+  <body>
+        <header>
+          <?php include 'components/barra-acessibilidade.php' ?>
+          <?php include 'components/barra-brasil.php' ?>
+          <?php include 'components/topoPrincipal.php' ?>
+        </header>
+
+        <main class="container">
+            <section id="breadcrumbs">Você está em: <a href="/sitiosead/editais">Blog</a> > <span>Galeria de Imagens</span></section>
+            <h1>Galeria de Imagens</h1>
+            
+            <section id="filtros">Navegue por ano: <button>2015</button><button>2016</button><button>2017</button><button>2018</button></section>
+            
+            <ul id="eventos">
+                <li>
+                    <h2><span>19/09/2018</span></h2>
+                    <p>I Semana Acadêmica e Cultural do Polo UAB de Iúna-ES</p>
+                    <ul class="imagens">
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>                                                                                              
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                    </ul>
+                    <button class="abrirTodas">Abrir todas</button>
+                </li>
+                
+                <li>
+                    <h2><span>07 a 11/11/2018</span></h2>
+                    <p>II Semana Acadêmica do Polo UAB de Montanha: “15 anos de EaD no Município de Montanha – ES”</p>
+                    <ul class="imagens">
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>                                                                                              
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                    </ul>
+                    <button class="abrirTodas">Abrir todas</button>
+                </li>
+                
+                <li>
+                    <h2><span>07 a 11/11/2018</span></h2>
+                    <p>II Semana Acadêmica do Polo UAB de Montanha: “15 anos de EaD no Município de Montanha – ES”</p>
+                    <ul class="imagens">
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>                                                                                              
+                        <li><img src="https://www.eadufes.org/site/wp-content/uploads/2018/06/IMG-20180621-WA0037.jpg"></li>
+                    </ul>
+                    <button class="abrirTodas">Abrir todas</button>
+                </li>
+            </ul>
+            
+        </main>
+        <?php include 'components/rodape.php' ?>
+      
+    <script defer="defer" src="//barra.brasil.gov.br/barra.js"></script>          
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="<?php echo site_url(); ?>/wp-content/themes/sead-v2/js/altoContraste.js"></script>
+  </body>
+</html>
