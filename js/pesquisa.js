a53034ba9a3c3569a4adb04f8b9956a453766944
natodp <renato714@outlook.com>
2018-09-25 14:54:56 -0300

    [Andamento] Barra de pesquisa

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,13 +1,54 @@
 // Em qualquer clique (rato ou teclado)
+//$(document).on('click keyup', function () {
+//
+//  // Se a pesquisa estiver em foco
+//  if ($('input').is(':focus')) {
+//
+//    // Esconde menu principal
+//    $('#menuPrincipal').hide()
+//    $('#formPesquisa').addClass('formPesquisaAtivo')
+//
+//  } else {
+//    $('#menuPrincipal').show()
+//    $('#formPesquisa').removeClass('formPesquisaAtivo')
+//  }
+//});
+
+$(document).on('click', function() {
+  
+  $('#formPesquisa').removeClass('formPesquisaAtivo');
+  
+})
+
+
+$('.botaoFalso').on('click', function () {
+
+  $('#formPesquisa').addClass('formPesquisaAtivo');
+  $('#formPesquisa input').focus();
+  return false;
+  
+});
+
+$('#formPesquisa input').on('click', function () {
+
+  $('#formPesquisa').addClass('formPesquisaAtivo');
+  $('#formPesquisa input').focus();
+  return false;
+  
+});
+
 $(document).on('click keyup', function () {
 
-  // Se a pesquisa estiver em foco
-  if ($('input').is(':focus')) {
+  if ($('#formPesquisa input').is(':focus')) {
 
-    // Esconde menu principal
-    $('#menuPrincipal').hide()
-
+    $('#formPesquisa').addClass('formPesquisaAtivo');
+    
+  } else if ($('#formPesquisa .botaoSubmissao').is(':focus')) {
+    
+    $('#formPesquisa').addClass('formPesquisaAtivo');
+    
   } else {
-    $('#menuPrincipal').show()
+    $('#formPesquisa').removeClass('formPesquisaAtivo');
   }
+    
 });
