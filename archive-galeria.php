b7b0364d1da2292deb7e88c55e865534ec2192b6
Unknown <isabelazucarato@designinstrucional.org>
2018-09-26 12:58:30 -0300

    Adicionando filtros na Galeria

--- archive-galeria.php
+++ archive-galeria.php
@@ -1,21 +1,43 @@
 <?php get_header('blog'); ?>
 
 <main id="page-galeria" class="container">
-  <section id="breadcrumbs">Você está em: <a href="/sitiosead/editais">Blog</a> > <span>Galeria de Imagens</span></section>
+  <section id="breadcrumbs">Você está em: <a href="/sitiosead/blog">Blog</a> > <span>Galeria de Imagens</span></section>
   <h1>Galeria de Imagens</h1>
 
-  <section id="filtros"><span>Navegue por ano: </span><button>2015</button><button>2016</button><button>2017</button><button>2018</button></section>
+  <section id="filtros">
+      <span>Navegue por ano: </span>
+      
+        <?php 
+            $terms = get_terms( array(
+                'taxonomy' => 'ano',
+                'oderby' => 'name',
+                'order' => 'DESC'
+            ) );
+            
+            $ano = isset($_GET['ano']) ? filter_var($_GET['ano'], FILTER_SANITIZE_URL) : '2018';
+            
+            
+            
+            foreach( $terms as $term ) {
+                $ativo = ($ano == $term->name) ? ' class="active" ' : '';
+                
+                echo '<a id="'.  $term->name .'" href="/sitiosead/galeria/?ano=' .  $term->name . '" '. $ativo .' >' . $term->name .'</a>';
+            }
+            
+        ?>
+
+  </section>
 
   <ul id="eventos">
-        <?php 
-                            
+        <?php                   
+                     
             $args = array(
                 'post_type' => 'galeria', 
                 'tax_query' => array( 
                     array( 
                         'taxonomy' => 'ano',
                         'field' => 'slug', 
-                        'terms' => '2018' 
+                        'terms' => $ano 
                     ) 
                 ) 
             );
@@ -28,19 +50,15 @@
                     
                     <p><?php the_excerpt(); ?></p>
                     
-                    <ul class="imagens" id="<?php the_ID(); ?>">
-                      <?php the_content() ?>
-                    </ul>
-                    
-                    <button class="abrirTodas" onclick="show()">Abrir todas</button>
+                    <ul class="imagens">
+                        <?php the_content() ?>
+                        
+                        <button class="abrirTodas" type="button">Abrir todas</button>
+                    </ul>                       
                 </li>
-                
-                <script>;
-                    function show(){
-                        $('#<?php the_ID(); ?> img:nth-child(n+7)').toggleClass("visivel");
-                    }
-                </script>
-            <?php endwhile; ?>            
+
+            <?php endwhile; ?>
+
                 <?php wp_reset_postdata(); ?>
             <?php else : ?>
                 <p><?php esc_html_e('Não há eventos cadastrados.'); ?></p>
@@ -49,3 +67,10 @@
 </main>
 
 <?php get_footer(); ?>
+
+                <script>
+                    $('.abrirTodas').click(e => {
+                        console.log('oi');
+                      $(e.target.parentElement).find('img:nth-of-type(n+7)').toggleClass("visivel");
+                    });
+                </script>
