67da6a0ecdf44e60314f860474f2b437afeba88c
Unknown <isabelazucarato@designinstrucional.org>
2018-10-02 12:15:30 -0300

    Desabilitando Lightbox na página Blog
    e direcionando o link 'ver mais' para a ancora do evento específico

--- archive-galeria.php
+++ archive-galeria.php
@@ -5,68 +5,65 @@
   <h1>Galeria de Imagens</h1>
 
   <section id="filtros">
-      <span>Navegue por ano: </span>
+    <span>Navegue por ano: </span>
       
-        <?php 
-            $terms = get_terms( array(
-                'taxonomy' => 'ano',
-                'oderby' => 'name',
-                'order' => 'DESC'
-            ) );
-            
-            $ano = isset($_GET['ano']) ? filter_var($_GET['ano'], FILTER_SANITIZE_URL) : '2018';
-         
-            foreach( $terms as $term ) {
-                $ativo = ($ano == $term->name) ? ' class="active" ' : '';
-                
-                echo '<a id="'.  $term->name .'" href="/sitiosead/galeria/?ano=' .  $term->name . '" '. $ativo .' >' . $term->name .'</a>';
-            }
-        ?>
+    <?php 
+      $terms = get_terms( array(
+        'taxonomy' => 'ano',
+        'oderby' => 'name',
+        'order' => 'DESC'
+      ) );
 
+      $ano = isset($_GET['ano']) ? filter_var($_GET['ano'], FILTER_SANITIZE_URL) : '2018';
+
+      foreach( $terms as $term ) {
+        $ativo = ($ano == $term->name) ? ' class="active" ' : '';
+
+        echo '<a id="'.  $term->name .'" href="/sitiosead/galeria/?ano=' .  $term->name . '" '. $ativo .' >' . $term->name .'</a>';
+      }
+    ?>
   </section>
 
   <ul id="eventos">
-        <?php                   
-                     
-            $args = array(
-                'post_type' => 'galeria', 
-                'tax_query' => array( 
-                    array( 
-                        'taxonomy' => 'ano',
-                        'field' => 'slug', 
-                        'terms' => $ano 
-                    ) 
-                ) 
-            );
+    <?php                   
+      $args = array(
+          'post_type' => 'galeria', 
+          'tax_query' => array( 
+              array( 
+                  'taxonomy' => 'ano',
+                  'field' => 'slug', 
+                  'terms' => $ano 
+              ) 
+          ) 
+      );
 
-            $eventos = new WP_Query( $args ); if ($eventos->have_posts()) : ?>  
-      
-            <?php while ($eventos->have_posts()) : $eventos->the_post(); ?>	
-                <li>
-                    <h2><span><?php the_title(); ?></span></h2>
-                    
-                    <p><?php the_excerpt(); ?></p>
-                    
-                    <ul class="imagens">
-                        <?php the_content() ?>
-                        
-                        <button class="abrirTodas" type="button">Abrir todas</button>
-                    </ul>                       
-                </li>
+      $eventos = new WP_Query( $args ); if ($eventos->have_posts()) : ?>  
 
-            <?php endwhile; ?>
+        <?php while ($eventos->have_posts()) : $eventos->the_post(); ?>	
+          <li id="evento-<?php the_ID(); ?>">
+            <h2><span><?php the_title(); ?></span></h2>
 
-                <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-                <p><?php esc_html_e('Não há eventos cadastrados.'); ?></p>
-        <?php endif; ?>     
+            <p><?php the_excerpt(); ?></p>
+
+            <ul class="imagens">
+                <?php the_content() ?>
+
+                <button class="abrirTodas" type="button">Abrir todas</button>
+            </ul>                       
+          </li>
+
+        <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+          <p><?php esc_html_e('Não há eventos cadastrados.'); ?></p>
+    <?php endif; ?>     
   </ul>
 </main>
 
 <?php get_footer(); ?>
 
-                <script>
-                    $('.abrirTodas').click(e => {
-                      $(e.target.parentElement).find('a:nth-of-type(n+7)').toggleClass("visivel");
-                    });
-                </script>
+<script>
+    $('.abrirTodas').click(e => {
+      $(e.target.parentElement).find('a:nth-of-type(n+7)').toggleClass("visivel");
+    });
+</script>
