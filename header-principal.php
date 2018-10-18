4ed972c4f72782a75fcf074c213d57f60e1b66ab
natodp <renato714@outlook.com>
2018-10-18 13:39:54 -0300

    Remoção de atalho para ligações telefônicas

--- header-principal.php
+++ header-principal.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
 
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="https://cdn.eadufes.org/icone/sead.png">
