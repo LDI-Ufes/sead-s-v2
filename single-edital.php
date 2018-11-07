54c4b24aa2c2b6e4f7cbcb5d551527fe16387735
Unknown <isabelazucarato@designinstrucional.org>
2018-11-07 08:36:30 -0200

    Removendo templates de edital e notícia

--- single-edital.php
+++ single-edital.php
@@ -1,16 +1,44 @@
 <?php get_header('principal'); ?>
  
-    <div id="primary" class="content-area">
+<?php while ( have_posts() ) : the_post(); ?>
  
-        <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
- 
-            get_template_part( 'edital', get_post_format() );
+  <main id="edital" class='container page'>
+    <!--Inserir conteúdo-->
+    <section class="breadcrumb">Você está em: <a href="/sitiosead/editais">Editais</a><i class="fas fa-greater-than"></i><span><?php the_title(); ?></span></section>
+    <h1>Edital <?php the_title(); ?></h1>
 
-        endwhile;
-        ?>
- 
-    </div><!-- .content-area -->
- 
+    <section id="informacoes">
+      <div>
+        <em>Curso: </em><?php echo strip_tags(get_the_term_list($post->ID, 'curso')); ?><br>
+        <em>Formação/nível: </em><?php echo strip_tags(get_the_term_list($post->ID, 'tipo-de-curso')); ?><br>
+        <em>Atribuição: </em><?php echo strip_tags(get_the_term_list($post->ID, 'atribuicao')); ?><br>
+      </div>
+      <div id="descricao">
+        <em>Descrição: </em><?php the_excerpt(); ?><br>
+      </div>
+    </section>
+
+    <section id="anexos" class="secao-edital">
+      <h2>Anexos</h2>
+      <?php the_content(); ?>
+    </section>
+
+    <?php
+    $editalLink = get_post_custom_values('edital-link');
+
+    if ($editalLink != "") {
+      echo "
+        <section id='inscricao' class='secao-edital'>
+          <h2>Inscrição</h2>
+          <p>Inscrições, retificações e resultados deste edital estão disponíveis no Sistema de Seleção.</p>
+          <a role='button' href=' " .$editalLink[0] . " ' target='_blank'>Acessar Sistema de Seleção</a>
+        </section>
+      ";
+    }
+    ?>
+
+  </main>
+
+<?php endwhile; ?>
+
 <?php  get_footer(); ?>
--- single-noticia.php
+++ single-noticia.php
@@ -1,16 +1,39 @@
 <?php get_header('blog'); ?>
  
-    <div id="primary" class="content-area">
- 
-        <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
+<?php while ( have_posts() ) : the_post(); ?>
 
-            get_template_part( 'noticia', get_post_format() );
+  <main id="noticia">
+    <div class="post-capa" style="background-image: linear-gradient(#043166, transparent), url('<?php echo get_the_post_thumbnail_url(); ?>');">
+    </div>
 
-        endwhile;
-        ?>
- 
-    </div><!-- .content-area -->
+    <article class="container">
+      <h1><?php the_title(); ?></h1>
+      <div class="post-date"><i class="fas fa-clock"></i><?php the_date('j \d\e F \d\e Y'); ?></div>
+
+      <div class="post-body">
+        <?php the_content(); ?>
+      </div>
+
+      <div class="post-autor">
+        <?php echo get_avatar(get_the_author_meta('ID')); ?>
+        <div class="autor-info">
+          postado por
+          <div class="autor-name"><?php the_author(); ?></div>
+          <div class="autor-role"><?php the_author_meta('cargo'); ?></div>
+          <div class="autor-bio"><?php the_author_meta('description'); ?></div>             
+        </div>
+      </div>
+
+      <div class="links-compartilhamento">
+        <span>Gostou do que leu?</span> Compartilhe essa notícia.
+        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="http://twitter.com/share?text=Ead na Ufes:&url=<?php echo get_permalink(); ?>" target="_blank"><i class="fab fa-twitter"></i></a>   
+        <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>" target="_blank"><i class="fab fa-linkedin"></i></a>      
+      </div>
+
+    </article>
+  </main>
+
+<?php endwhile; ?>
  
 <?php get_footer(); ?>
