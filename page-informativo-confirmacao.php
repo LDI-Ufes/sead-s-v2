416bac1d8d83d7d4a92852a177acbd86a70a3d8c
natodp <renato714@outlook.com>
2018-11-14 14:29:23 -0200

    Informativos: Páginas de confirmação e problema com inscrição

---
+++ page-informativo-confirmacao.php
@@ -0,0 +1,14 @@
+<?php get_header('principal'); ?>
+
+<main id="informativo-confirmacao" class="container page">
+
+  <h1>Parabéns</h1>
+
+  <section>
+    Agora sim, sua inscrição para receber nossos informativos está completa. <br> Mais uma vez, agradecemos o interesse. <br>
+    <a href="<?php echo site_url(); ?>/blog">Ir para nosso Blog <i class="fas fa-angle-double-right"></i></a>
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
