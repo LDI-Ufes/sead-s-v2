cec864f3f4b7338a3dcf0fe1c2dc83eda61e8b9b
natodp <renato714@outlook.com>
2018-11-20 13:27:56 -0200

    Remoção da pasta Arquivos da sincronização

--- components/barra-acessibilidade.php
+++ components/barra-acessibilidade.php
@@ -1,21 +1,21 @@
-<div class="atalhos-navegacao">
-  <a href="" class="atalho-navegacao"><span>Ir para o conteúdo</span></a>
-  <a href="" class="atalho-navegacao"><span>Pular para o menu principal</span></a>
+<div class='atalhos-navegacao'>
+  <a href='' class='atalho-navegacao'><span>Ir para o conteúdo</span></a>
+  <a href='#topoPrincipal' class='atalho-navegacao'><span>Pular para o menu principal</span></a>
 </div>
 
-<div id="barraAuxiliar">
-  <section class="caixa">
-    <span class="btoMenu">Endereços úteis <i class="fas fa-chevron-down"></i></span>
-    <nav class="menu">
-      <ul id="ligacoesExternas" class="menuLista">
+<div id='barraAuxiliar'>
+  <section class='caixa'>
+    <span class='btoMenu'>Endereços úteis <i class='fas fa-chevron-down'></i></span>
+    <nav class='menu'>
+      <ul id='ligacoesExternas' class='menuLista'>
         <li>
-          <a href="<?php echo site_url(); ?>/faq">Dúvidas frequentes</a>
+          <a href='<?php echo site_url(); ?>/faq'>Dúvidas frequentes</a>
         </li>
-        <li class="temSubmenu">
-          <span>AVA Moodle <i class="fas fa-chevron-down"></i></span>
-          <ul class="menuSuspenso">
-            <li><a href="http://www.ead.ufes.br" target="_blank" title="AVA Moodle de Graduação">Graduação</a></li>
-            <li><a href="http://www.especializacao.aperfeicoamento.ufes.br" target="_blank" title="AVA Moodle de Especialização e Aperfeiçamento">Especialização e Aperfeiçoamento</a></li>
+        <li class='temSubmenu'>
+          <span>AVA Moodle <i class='fas fa-chevron-down'></i></span>
+          <ul class='menuSuspenso'>
+            <li><a href='http://www.ead.ufes.br' target='_blank' title='AVA Moodle de Graduação'>Graduação</a></li>
+            <li><a href='http://www.especializacao.aperfeicoamento.ufes.br' target='_blank' title='AVA Moodle de Especialização e Aperfeiçamento'>Especialização e Aperfeiçoamento</a></li>
           </ul>
         </li>
       </ul>
