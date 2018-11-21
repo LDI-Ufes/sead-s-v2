65b701b64698ed67be34c006546c65aa54a27f68
natodp <renato714@outlook.com>
2018-11-21 09:06:50 -0200

    Fechar atalhos de navegação com Esc

--- js/globais.js
+++ js/globais.js
@@ -1,3 +1,8 @@
 
 // Plugin To Top
 $("#to_top_scrollup").prop("title", "Voltar ao topo")
+
+// Fechar atalhos de navegação com Esc
+$(document).keyup(function(e) {
+  if (e.keyCode === 27) window.open('#barra-brasil', '_self')
+});
