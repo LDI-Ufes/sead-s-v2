e79bf7b8618c10ff0e9552cf1be24b7f8d0b3526
natodp <renato714@outlook.com>
2018-11-30 17:47:36 -0200

    [Andamento] Revisão de ligações acessíveis
    Revisão dos critérios de title, texto auxiliar e iconografia

--- components/barra-acessibilidade.php
+++ components/barra-acessibilidade.php
@@ -14,8 +14,8 @@
         <li class='temSubmenu'>
           <span>AVA Moodle <i class='fas fa-chevron-down'></i></span>
           <ul class='menuSuspenso'>
-            <li><a href='http://www.ead.ufes.br' target='_blank' title='Abrir o AVA Moodle de Graduação em nova aba'>Graduação</a></li>
-            <li><a href='http://www.especializacao.aperfeicoamento.ufes.br' target='_blank' title='Abrir o AVA Moodle de Especialização e Aperfeiçamento em nova aba'>Especialização e Aperfeiçoamento</a></li>
+            <li><a href='http://www.ead.ufes.br' target='_blank' title='Abrir o AVA Moodle de Graduação em nova aba'><span class="somente-leitura">AVA Moodle de</span> Graduação <i class="fas fa-external-link-alt"></i></a></li>
+            <li><a href='http://www.especializacao.aperfeicoamento.ufes.br' target='_blank' title='Abrir o AVA Moodle de Especialização e Aperfeiçamento em nova aba'><span class="somente-leitura">AVA Moodle de</span> Especialização e Aperfeiçoamento <i class="fas fa-external-link-alt"></i></a></li>
           </ul>
         </li>
       </ul>
