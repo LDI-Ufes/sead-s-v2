44384aaf42b023d3492a20c0d0b37a67097d259b
Unknown <isabelazucarato@designinstrucional.org>
2018-12-17 12:03:36 -0200

    Atualizando conteúdo do FAQ

--- page-faq.php
+++ page-faq.php
@@ -6,37 +6,25 @@
 
   <ul>
     <li>
-      <button><h2>O diploma/certificado EAD é válido?</h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
+      <button><h2>O que é um curso a distância semipresencial?</h2></button>
+      <p>É um curso feito na modalidade de ensino a distância, mas que exige a presença do discente no polo de apoio presencial ao ensino a distância em datas pré-fixadas para realização de encontros presenciais, aulas experimentais, avaliações, entrega de trabalho entre outras solicitações.</p>
     </li>
     <li>
-      <button><h2>Como funcionam as aulas e os materiais didáticos? </h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
+      <button><h2>Os encontros presencias são obrigatórios? </h2></button>
+      <p>O aluno deve possuir uma frequência igual ou superior a 75% da carga horária contabilizada para os encontros presenciais. As monitorias extras são opcionais e não são contabilizadas nesse cálculo.</p>
+    </li>
+    <li>
+      <button><h2>O que é um tutor a distância e um tutor presencial?</h2></button>
+      <p>O tutor a distância é o mediador entre o professor, os tutores presenciais e os alunos dos polos. Já o tutor presencial é responsável pelo acompanhamento direito e sistemático dos alunos nos polos.</p>
     </li>      
     <li>
-      <button><h2>Posso fazer mais de um curso ao mesmo tempo? </h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
+      <button><h2>É possível realizar a transferência de curso da modalidade presencial para a modalidade a distancia e vice-versa?</h2></button>
+      <p>Sim. Será assegurada a transferência de alunos da modalidade presencial para a modalidade a distância e vice-versa, conforme edital do Processo Seletivo para Vagas Surgidas. Maiores informações podem ser obtidas no <a href="http://www.psvs.ufes.br" target="_blank">sítio do PSVS Ufes</a>.</p>
     </li>      
     <li>
-      <button><h2>Onde posso entrar em contato para tirar outras dúvidas? </h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
+      <button><h2>É possível aproveitar disciplinas cursadas na modalidade presencial, para a modalidade a distância?</h2></button>
+      <p>Sim. Será assegurado o aproveitamento de disciplina cursada na modalidade presencial ou em outros cursos da modalidade a distância, desde que haja equivalência de conteúdos e de carga horária das disciplinas cursadas.</p>
     </li>     
-    <li>
-      <button><h2>O curso é totalmente a distância? </h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
-    </li>     
-    <li>
-      <button><h2>Como funciona o processo de avaliação?</h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
-    </li>
-    <li>
-      <button><h2>Como funciona o Trabalho de Conclusão de Curso (TCC)?</h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
-    </li>
-    <li>
-      <button><h2>Quais são as vantagens de estudar na modalidade a distância?</h2></button>
-      <p>Todo o material didático é disponibilizado on-line, por meio de recursos multimídia, podcasts, vídeos, videoaulas, animações, objetos de aprendizagem e-books, webconferências, no Ambiente Virtual de Aprendizagem. Caso prefira, é possível realizar a impressão.</p>
-    </li>
   </ul>
 </main>
 
