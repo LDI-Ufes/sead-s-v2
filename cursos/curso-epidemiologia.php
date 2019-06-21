255a2d6df1442d13b2be8c8e3b7e5db7cfa61521
Isabela <isabelazucarato@designinstrucional.org>
2019-06-21 09:12:59 -0300

    Curso Epidemiologia

---
+++ cursos/curso-epidemiologia.php
@@ -0,0 +1,84 @@
+<main class='page curso' id="conteudoPrincipal">
+
+  <section class="destaque destaque-imagem" style="background-image: linear-gradient(#043166, rgba(4, 49, 102, 0.3)), url('<?php echo get_template_directory_uri() ?>/img/capas/epidemiologia.jpg')"></section>;
+
+  <div class='container'>
+    <section class='breadcrumb'>Você está em: <a href='<?php echo site_url(); ?>/cursos'>Cursos</a><i class='fas fa-greater-than'></i><span>Epidemiologia</span></section>
+    <h1>Especialização em Epidemiologia EaD</h1>
+
+    <p id='objetivo'>Promover mudanças não só pela incorporação de conhecimento, mas também ampliando a capacidade crítica dos especializandos na busca de soluções em seus serviços, sobretudo pela ideia de que o fortalecimento da capacidade de atuação na área epidemiológica, que implica no domínio de um instrumental básico nas áreas privilegiadas e na possibilidade dos especializandos vivenciarem experiências inovadoras que façam sentido na realidade em que estão inseridos no cotidiano de suas instituições.</p>
+
+    <section id='info-curso'>
+      <div>
+        <p><strong>Nível: </strong>Especialização</p>
+        <p><strong>Duração: </strong>18 meses</p>
+        <p><strong>Modalidade: </strong>Semipresencial</p>
+      </div>
+
+      <div id='situacao'>
+        <p><strong>Situação: </strong>Oferta 2016 concluída.<br/>
+          <strong>Sem previsão de reoferta.</strong>
+        </p>
+      </div>
+    </section>
+
+    <section id='conteudo'>
+      <div id='matriz'>
+        <h2 id='curriculo'>Duração do curso</h2>
+        <p>Carga horária total: 420 horas<br/>
+			Módulo I – Bases Introdutórias (135 horas)<br/>
+			Módulo II – Métodos e Técnicas Utilizadas em Epidemiologia (135 horas)<br/>
+			Módulo III – Bases Operacionais (75 horas)<br/> 
+			Módulo IV – A Epidemiologia no Planejamento dos Serviços (75 horas)
+        </p>
+
+        <h2>Matriz Curricular</h2>
+
+        <ul>
+          <li class='semestre'>
+            <button class='ativo'><h3>Módulo I – Bases Introdutórias</h3></button>
+            <ul style='display: block;'>
+              <li>As Políticas Públicas de Saúde <span>30h</span></li>
+              <li>Metodologia do trabalho científico <span>30h</span></li>
+              <li>Introdução a Epidemiologia <span>15h</span></li>
+              <li>Estatística Básica <span>30h</span></li>
+              <li>Demografia Básica <span>30h</span></li>
+            </ul>
+          </li>
+          
+          <li class='semestre'>
+            <button class='ativo'><h3>Módulo II – Métodos e Técnicas Utilizadas em Epidemiologia</h3></button>
+            <ul style='display: block;'>
+
+              <li>Introdução ao Pacote Estatístico EPI <span>15h</span></li>
+              <li>Estudos Descritivos <span>30h</span></li>
+              <li>Estudos Analíticos <span>30h</span></li>
+              <li>Tecnologia em Saúde  <span>30h</span></li>
+              <li>Estudo Monográfico <span>30h</span></li>
+            </ul>
+          </li>
+          
+          <li class='semestre'>
+            <button class='ativo'><h3>Módulo III – Bases Operacionais</h3></button>
+            <ul style='display: block;'>
+
+              <li>Doenças Transmissíveis e Modos de Transmissão  <span>15h</span></li>
+              <li>Doenças Não-Transmissíveis  <span>15h</span></li>
+              <li>Vigilância em Saúde e Qualidade de Vida  <span>30h</span></li>
+              <li>Estudo Monográfico <span>15h</span></li>
+            </ul>
+          </li>
+
+          <li class='semestre'>
+            <button class='ativo'><h3>Módulo IV – A Epidemiologia no Planejamento dos Serviços</h3></button>
+            <ul style='display: block;'>
+
+              <li>Qualidade dos Serviços e Programas de Saúde <span>30h</span></li>
+              <li>Políticas Setoriais de Saúde <span>30h</span></li>
+              <li>Estudo Monográfico <span>15h</span></li>
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+</main>
