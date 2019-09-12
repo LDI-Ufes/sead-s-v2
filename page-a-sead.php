0b4835da658df2e335c4ee3929971219a22ef1db
Isabela <isabelazucarato@designinstrucional.org>
2019-09-12 12:29:33 -0300

    Reordenação dos parágrafos da apresentação da Sead

--- page-a-sead.php
+++ page-a-sead.php
@@ -30,15 +30,14 @@
     <div>
       <p>A Universidade Federal do Espírito Santo – Ufes, ao longo de sua história, tem formado geração de profissionais e, por isso, conquistou respeito e credibilidade junto à sociedade espírito-santense. A partir de 2001,  credenciou-se junto ao Ministério da Educação – MEC para a oferta de cursos superiores na modalidade de EaD.</p>
       <p>Para dar suporte a essa iniciativa, a Ufes introduziu, na sua estrutura organizacional, o Núcleo de Educação Aberta e a Distância – ne@ad. Ao longo dos anos, o ne@ad consolidou-se como órgão mediador e gestor das propostas de EaD, tornando-se responsável pela introdução sistematizada dessa cultura na Universidade, bem como  pela mediação, organização, oferta de cursos e demais atividades acadêmicas à população espírito-santense.</p>
+      <p>O primeiro curso EaD da Ufes foi o de Pedagogia, por meio do qual formaram-se 6.171 professores das séries iniciais do ensino fundamental dos sistemas públicos de ensino dos 78 municípios capixabas, que atuavam sem formação em nível superior.</p>
+      <p>Em 2006, a Universidade fez adesão ao projeto piloto do curso de Administração, a distância,  do Sistema Universidade Aberta do Brasil – UAB. Atualmente oferta cursos EaD em 27 Polos Municipais de Apoio Presencial –  UAB. Assim, a Ufes apresenta-se como parceira institucional no desenvolvimento do ensino, pesquisa, formação continuada e demais ações direcionadas à formação de cidadãos deste estado.</p>      
       <p>A partir de abril/2014, por meio da Resolução nº 08/2014 do Conselho Universitário, foi aprovada a reestruturação organizacional da Ufes. Nessa nova estrutura, o ne@ad passou a ser denominado “Secretaria de Ensino a Distância” – Sead.</p>
       <p>Em agosto/2019, entrou em vigor a nova reestruturação organizacional da Ufes e as adaptações administrativas resultaram na modificação de nomes de alguns setores, conforme suas funções. A Secretaria de Ensino a Distância – Sead passou a ser denominada “Superintendência de Educação a Distância”, mantendo-se a sigla Sead. Essa alteração foi aprovada no mês de julho pelo Conselho Universitário por meio da Resolução nº 22/2019.</p>
-      <p>Em 2006, a Universidade fez adesão ao projeto piloto do curso de Administração, a distância,  do Sistema Universidade Aberta do Brasil – UAB. Atualmente oferta cursos EaD em 27 Polos Municipais de Apoio Presencial –  UAB. Assim, a Ufes apresenta-se como parceira institucional no desenvolvimento do ensino, pesquisa, formação continuada e demais ações direcionadas à formação de cidadãos deste estado.</p>
       <blockquote>Nesse contexto, a  proposta de EaD da Ufes visa a intensificar a cultura da educação aberta e a distância na Universidade, bem como a utilização das novas tecnologias da comunicação e da informação nos espaços formadores internos.</blockquote>
-      <p>O primeiro curso EaD da Ufes foi o de Pedagogia, por meio do qual formaram-se 6.171 professores das séries iniciais do ensino fundamental dos sistemas públicos de ensino dos 78 municípios capixabas, que atuavam sem formação em nível superior.</p>
-
     </div>
 
-    <section id="linha-do-tempo"></section>
+    <!-- <section id="linha-do-tempo"></section> -->
 
     <section id="estrutura">
       <h2>Relação de Dirigentes</h2>
