d525b337c5776dc1a696bcdcc9659a3cbf0ef222
Isabela <isabelazucarato@gmail.com>
2020-10-20 13:51:29 -0300

    Atualizando páginas de cursos

--- cursos/curso-genero-e-diversidade-na-escola.php
+++ cursos/curso-genero-e-diversidade-na-escola.php
@@ -1,119 +1,138 @@
 <main class='page curso' id="conteudoPrincipal">
 
-  <section class="destaque destaque-imagem" style="background-image: linear-gradient(#043166, rgba(4, 49, 102, 0.3)), url('<?php echo get_template_directory_uri() ?>/img/capas/genero-e-diversidade-na-escola.jpg')"></section>
+  <section class="destaque destaque-imagem" style="background-image: linear-gradient(#043166, rgba(4, 49, 102, 0.3)), url('<?php echo get_template_directory_uri()?>/img/capas/genero-e-diversidade-na-escola.jpg')"></section>
 
-  <div class='container'>
-    <section class='breadcrumb'>Você está em: <a href='<?php echo site_url(); ?>/cursos'>Cursos</a><i class='fas fa-greater-than'></i><span>Gênero e Diversidade na Escola</span></section>
-    <h1>Especialização em Gênero e Diversidade na Escola EaD</h1>
+  <section id='info-curso'> 
+    <div class="container"> 
+      <div class='breadcrumb'>Você está em: <a href='<?php echo site_url(); ?>/cursos'>Cursos</a><i class='fas fa-greater-than'></i><span>Gênero e Diversidade na Escola</span></div> 
+ 
+      <h1>Gênero e Diversidade na Escola</h1> 
+ 
+      <div class="colunas"> 
+ 
+        <p class='coluna1 objetivo'>O objetivo da Especialização em Gênero e Diversidade na Escola EaD é realizar uma formação continuada em gênero, raça/etnia e orientação sexual para professores, professoras e demais profissionais da Educação Básica.</p> 
+ 
+        <div class="coluna2"> 
+          <p> 
+            <strong>Nível: </strong>Especialização<br/> 
+            <strong>Modalidade: </strong>Semipresencial<br/> 
+          </p> 
+          <p class="situacao"><strong>Situação: </strong> 
+            <br><strong>Sem previsão de nova oferta.</strong> 
+          </p> 
+        </div> 
+      </div> 
+    </div> 
+  </section> 
+ 
+    <section id='conteudo-curso' class='container colunas'> 
+ 
+      <aside class="coluna2"> 
+         
+        <div class='menu'> 
+          <h4>Navegue</h4> 
+ 
+          <ul> 
+            <li><a title="Mostrar currículo" href='#curriculo'>Grade curricular</a></li> 
+            <li><a title="Mostrar ofertas do curso" href='#ofertas-anteriores'>Ofertas</a></li> 
+          </ul> 
+        </div> 
+ 
+      </aside> 
+ 
+      <div class="coluna1"> 
+ 
+ 
+        <h2 id='curriculo'>Grade curricular</h2> 
+        <p>Carga horária total: 380 horas</p> 
+ 
+        <ul id="matriz"> 
+            <li class='semestre'> 
+              <button><h3>Módulo I - Orientações Gerais</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>Orientações</div> 
+                  </li> 
+                  <li> 
+                    <div>Diversidade</div> 
+                  </li> 
+              </ul> 
+            </li>
+            <li class='semestre'> 
+              <button><h3>Módulo II - Gênero</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>Gênero: um conceito importante para o conhecimento do mundo social</div> 
+                  </li> 
+                  <li> 
+                    <div>A importância dos movimentos sociais na luta contra as desigualdades de gênero</div> 
+                  </li> 
+                  <li> 
+                    <div>Gênero no cotidiano escolar</div> 
+                  </li> 
+              </ul> 
+            </li>
+            <li class='semestre'> 
+              <button><h3>Módulo III - Sexualidade e Orientação Sexual</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>Sexualidade: dimensão conceitual, diversidade, discriminação</div> 
+                  </li> 
+                  <li> 
+                    <div>Saúde, sexualidade e reprodução</div> 
+                  </li> 
+                  <li> 
+                    <div>Sexualidade no cotidiano escolar</div> 
+                  </li> 
+              </ul> 
+            </li>
+            <li class='semestre'> 
+              <button><h3>Módulo IV - Relações Etnicorraciais</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>Noções de raça, racismo e etnicidade</div> 
+                  </li> 
+                  <li> 
+                    <div>Desigualdade racial</div> 
+                  </li> 
+                  <li> 
+                    <div>Igualdade étnico-racial também se aprende na escola</div> 
+                  </li> 
+              </ul> 
+            </li>
+            <li class='semestre'> 
+              <button><h3>Módulo V - A diversidade e o contexto do Espírito Santo</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>A realidade capixaba para as relações etnicorraciais</div> 
+                  </li> 
+                  <li> 
+                    <div>A realidade capixaba para as questões de gênero</div> 
+                  </li> 
+                  <li> 
+                    <div>A realidade capixaba para a diversidade sexual</div> 
+                  </li> 
+              </ul> 
+            </li>
+            <li class='semestre'> 
+              <button><h3>Módulo VI</h3></button> 
+              <ul> 
+                  <li> 
+                    <div>Avaliação</div> 
+                  </li> 
+              </ul> 
+            </li>
+        </ul> 
 
-    <p id='objetivo'>Realizar uma formação continuada em gênero, raça/etnia e orientação sexual para professores, professoras e demais profissionais da Educação Básica.</p>
+        <h2 id='ofertas-anteriores'>Ofertas do curso</h2> 
+ 
+        <!-- <h3>2008</h3>  -->
+        <p><strong>Coordenação do Curso</strong><br/>Antonio Lopes de Souza Neto</p> 
+        <p><strong>Polos atendidos</strong><br/>Afonso Cláudio, Aracruz, Colatina, Conceição da Barra, Domingos Martins, Venda Nova do Imigrante, Vila Velha, Vitória.</p> 
 
-    <section id='info-curso'>
-      <div>
-        <p><strong>Nível: </strong>Especialização</p>
-        <p><strong>Modalidade: </strong>Semipresencial</p>
-      </div>
-
-      <div id='situacao'>
-        <p><strong>Situação: </strong>
-          <br><strong>Sem previsão de reoferta.</strong>
-        </p>
-      </div>
-    </section>
-
-
-    <section id='conteudo'>
-
-<!--      <aside id='menu'>
-        <button id='dropdown-menu'>Menu<i class='fas fa-angle-down'></i></button>
-
-        <ul>
-          <li><a href='#curriculo' alt="Mostrar currículo do curso">Currículo</a></li>
-          <li><a href='#ofertas-anteriores' alt="Mostrar ofertas do curso">Ofertas</a></li>
-        </ul>
-
-
-      </aside>-->
-
-      <div id='matriz'>
-        <h2 id='curriculo'>Duração do curso</h2>
-        <p>Carga horária total: 380 horas</p>
-
-        <h2>Matriz Curricular</h2>
-
-        <ul>
-          <li class='semestre'>
-            <button class='ativo'><h3>Módulo I - Orientações Gerais</h3></button>
-            <ul style='display: block;'>
-
-              <li>Orientações</li>
-              <li>Diversidade </li>
-
-            </ul>
-          </li>
-          
-          <li class='semestre'>
-            <button><h3>Módulo II - Gênero</h3></button>
-            <ul>
-
-              <li>Gênero: um conceito importante para o conhecimento do mundo social</li>
-              <li>A importância dos movimentos sociais na luta contra as desigualdades de gênero</li>
-              <li>Gênero no cotidiano escolar </li>
-
-            </ul>
-          </li>
-          
-          <li class='semestre'>
-            <button><h3>Módulo III - Sexualidade e Orientação Sexual</h3></button>
-            <ul>
-
-              <li>Sexualidade: dimensão conceitual, diversidade, discriminação</li>
-              <li>Saúde, sexualidade e reprodução</li>
-              <li>Sexualidade no cotidiano escolar</li>
-
-            </ul>
-          </li>
-          
-          <li class='semestre'>
-            <button><h3>Módulo IV - Relações Etnicorraciais</h3></button>
-            <ul>
-
-              <li>Noções de raça, racismo e etnicidade </li>
-              <li>Desigualdade racial</li>
-              <li>Igualdade étnico-racial também se aprende na escola</li>
-
-            </ul>
-          </li>
-          
-          <li class='semestre'>
-            <button><h3>Módulo V - A diversidade e o contexto do Espírito Santo</h3></button>
-            <ul>
-              <li>A realidade capixaba para as relações etnicorraciais</li>
-              <li>A realidade capixaba para as questões de gênero</li>
-              <li>A realidade capixaba para a diversidade sexual</li>
-
-            </ul>
-          </li>
-          
-          <li class='semestre'>
-            <button><h3>Módulo VI</h3></button>
-            <ul>
-              <li>Avaliação</li>
-            </ul>
-          </li>
-        </ul>
-
-      </div>
-
-<!--      <div>
-        <h2 id='ofertas-anteriores'>Ofertas do curso</h2>
-
-        <h3></h3>
-        <p><strong>Coordenação de Curso: </strong>Antonio Lopes de Souza Neto </p>
-        <p><strong>Polos atendidos: </strong>Afonso Cláudio, Aracruz, Colatina, Conceição da Barra, Domingos Martins, Venda Nova do Imigrante, Vila Velha, Vitória.</p>
-
-      </div>-->
-    </section>
-  </div>
+ 
+    </section> 
+ 
+  </div> 
 
 </main>
